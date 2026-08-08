345bec416aab68d096d9000c93e5f7d68077ad81
Leon <vinneyto@gmail.com>
2026-08-08 12:54:28 +0300

    Format Python sources with Ruff

--- 08_asynchronous/02_concurrent_map/task.py
+++ 08_asynchronous/02_concurrent_map/task.py
@@ -5,9 +5,7 @@ T = TypeVar("T")
 R = TypeVar("R")
 
 
-async def concurrent_map(
-    func: Callable[[T], Awaitable[R]], values: Iterable[T]
-) -> list[R]:
+async def concurrent_map(func: Callable[[T], Awaitable[R]], values: Iterable[T]) -> list[R]:
     """Run func for all values concurrently and preserve input order."""
     # TODO: создайте корутины для всех значений и передайте их в asyncio.gather().
     raise NotImplementedError
